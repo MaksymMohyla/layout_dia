3a55087a923d0521eb5dda4a2d31a913736ab6a5
Maksym Mohyla <greenparksquad@gmail.com>
2024-07-27 13:03:38 +0300

    quicksave

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const switcher = document.querySelector('.theme-switcher');
+
+switcher.addEventListener('click', () => {
+  if (page.classList.contains('page--themeblue')) {
+    page.classList.remove('page--themeblue');
+    switcher.classList.remove('theme-switcher--themeblue');
+  } else {
+    page.classList.add('page--themeblue');
+    switcher.classList.add('theme-switcher--themeblue');
+  }
+});
